2482b4aa67fe4ba75baabb22fc765fd8dad2315e
ArulMC123 <99807639+ArulMC123@users.noreply.github.com>
2022-04-25 20:48:01 +0700

    Update groupowner.js

--- src/groupowner.js
+++ src/groupowner.js
@@ -1,9 +1,9 @@
 exports.groupowner = (prefix) => {
 return `*- LINK GROUP OWNER BOT*
 
-1. https://chat.whatsapp.com/G1PlSE3VR6e99hmMaxzpkg
-2. https://chat.whatsapp.com/LV3DOUhJerlHBkvAjcB5zr
-3. https://chat.whatsapp.com/LNCchYC5Iyp2VLpqy0oK7C
+1. Khusus Anak Minecraft : https://chat.whatsapp.com/DPcaEy3Ro4t34bF10uvnKu
+2. Khusus Youtube : https://chat.whatsapp.com/Cq2Rqvd5POa4HSOnAH0QLQ
+3. Subrek Youtube : Arul MINIKREP & Adly Editz
 
 _Jika mau bergabung jangan lupa baca deskripsi!!_`
 }
